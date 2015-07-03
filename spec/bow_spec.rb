5a1836347a35176b5fa15dd39c9fe7aad6dcefb5
Joseph Lozano <lozanojoseph@gmail.com>
2015-07-03 09:38:25 -0700

    start bow_spec.rb

--- spec/bow_spec.rb
+++ spec/bow_spec.rb
@@ -1,1 +1,62 @@
 # Your code here
+
+
+require 'viking'
+
+
+describe Bow do 
+
+  let(:bow){Bow.new}
+  let(:bow_five){Bow.new(5)}
+
+
+  describe '#arrows' do 
+
+
+    it 'should be able to read the arrow count' do
+
+      expect(bow.arrows).to be_a(Integer)
+
+    end
+
+    it 'should default to 10 arrows' do
+
+      expect(bow.arrows).to eql(10)
+
+    end
+
+
+    it 'should specify the number of arrows' do
+
+      expect(bow_five.arrows).to eql(5)
+
+    end
+    
+  end
+
+
+  describe '#use' do
+
+    it 'should reduce the number of arrows by 1' do
+
+
+
+    end
+
+
+  end
+
+  
+end
+
+
+
+
+
+
+
+
+
+
+
+
